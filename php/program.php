883533762d0b27cf926773eac344fd7f5dcf120d
Nguyen Phan Du <dunp@omt.vn>
2022-06-18 16:59:54 +0700

    define('__APP_ROOT_DIR__', dirname(__FILE__));

--- php/program.php
+++ php/program.php
@@ -1,19 +1,21 @@
 <?php
-define('__ROOT__', dirname(__FILE__));
+define('__APP_ROOT_DIR__', dirname(__FILE__));
 
-echo "RootDir: " . __ROOT__ . "\r\n";
+$workingDir = str_replace("\\", "/", __APP_ROOT_DIR__);
 
-require_once  __ROOT__ . '/vendor/autoload.php';
+echo "RootDir: " . $workingDir . "\r\n";
+
+require_once  $workingDir . '/vendor/autoload.php';
 echo "autoload.php init done\r\n";
 
 use app\RedisConnect;
 use app\RedisConsumer;
 use app\RedisProducer;
 
-$redis= new RedisConnect("localhost",6379,"",0);
+$redis = new RedisConnect("localhost", 6379, "", 0);
 
-$consumer1= new RedisConsumer($redis,"php1","dunp",function($msg){
-    echo "\r\n----inside consumer php1: ". $msg ."---\r\n";
+$consumer1 = new RedisConsumer($redis, "php1", "dunp", function ($msg) {
+    echo "\r\n----inside consumer php1: " . $msg . "---\r\n";
 });
 
 $consumer1->Start();
