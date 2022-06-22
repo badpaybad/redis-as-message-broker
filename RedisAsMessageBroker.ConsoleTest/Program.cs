ba2f97c9b083a82826595d4d18d439edb4bf154c
Nguyen Phan Du <dunp@omt.vn>
2022-06-22 16:21:52 +0700

    redis spam

--- RedisAsMessageBroker.ConsoleTest/Program.cs
+++ RedisAsMessageBroker.ConsoleTest/Program.cs
@@ -1,15 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using RedisAsMessageBroker.Core;
 
 RedisConnect redis = new RedisConnect("localhost", 6379);
 
 _ = Task.Run(() =>
 {
-
     var c1 = new RedisConsumer(redis, "dunp1", "dunp", async (msg) =>
      {
          await Task.Yield();
-         Console.WriteLine("consummer1: " + msg);
+         var obj = JsonConvert.DeserializeObject<TestMsg>(msg);
+         Console.WriteLine(obj.ToString());
          // you code business here
      });
 
@@ -17,38 +18,61 @@ _ = Task.Run(() =>
 
 });
 
-var c2 = new RedisConsumer(redis, "dunp2", "dunp", async (msg) =>
- {
-     await Task.Yield();
-     Console.WriteLine("consummer2: " + msg);
-     // you code business here
- });
+//var c2 = new RedisConsumer(redis, "dunp2", "dunp", async (msg) =>
+// {
+//     await Task.Yield();
+//     Console.WriteLine("consummer2: " + msg);
+//     // you code business here
+// });
 
-_ = c2.Start();
+//_ = c2.Start();
 
 long msgIdx = 0;
 
 var producer = new RedisProducer(redis);
 
-var stressTest = Enumerable.Range(0, 1000);
+var stressTest = Enumerable.Range(0, 100);
 Console.WriteLine("Start stress test");
+
+_ = Task.Run(async () => {
+
+    while (true)
+    {
+        Parallel.ForEach(stressTest, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+            async i =>
+            {
+                await producer.Publish("dunp",
+                JsonConvert.SerializeObject(new TestMsg
+                  {
+                      CreatedAt = DateTime.Now,
+                      Msg = " Nguyen Phan Du "
+                  })
+                );
+            });
+
+        msgIdx++;
+
+        await Task.Delay(1);
+    }
+
+});
 
 while (true)
 {
-    List<Task> tasks = new List<Task>();    
-
-    //Parallel.ForEach(stressTest, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
-    //    i =>
-    //    {
-    //        tasks.Add( producer.Publish("dunp", i + "__" + DateTime.Now.ToString()));
-            
-    //    });
-
-    tasks.Add( producer.Publish("dunp", "from c#: "+ msgIdx + "_" + DateTime.Now.ToString()));
-
-    msgIdx++;
-
-    await Task.WhenAll(tasks);
-
     await Task.Delay(1000);
+}
+
+public class TestMsg
+{
+    static long id = 0;
+
+    public long Id { get; set; } = id++;
+
+    public string Msg { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public override string ToString()
+    {
+        return $"Received after: {(DateTime.Now - CreatedAt).TotalMilliseconds} id={id} : {CreatedAt}";
+    }
 }
